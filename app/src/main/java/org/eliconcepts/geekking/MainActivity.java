a6f7f1f23cc0aa0790877de5ebd0ded4c5100da5
sommmy <somtochukwuanunobi@gmail.com>
2018-04-30 12:32:51 +0100

    Updates
    Made changes to build SDK, fixed issue with background remapping on
    keyboard pop up

--- app/src/main/java/org/eliconcepts/geekking/MainActivity.java
+++ app/src/main/java/org/eliconcepts/geekking/MainActivity.java
@@ -1,10 +1,10 @@
 package org.eliconcepts.geekking;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
